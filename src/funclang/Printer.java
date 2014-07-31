8d9c91588c8477baf3046abe8092f2ce924fdd9b
Hridesh Rajan <hridesh@cs.iastate.edu>
2014-07-31 17:12:47 -0500

    Adding a converter to string.

--- src/funclang/Printer.java
+++ src/funclang/Printer.java
@@ -1,10 +1,93 @@
 package funclang;
 
+import java.util.List;
+
+import funclang.AST.Exp;
+
 public class Printer {
 	public void print(Value v) {
 		System.out.println(v.tostring());
 	}
 	public void print(Exception e) {
 		System.out.println(e.toString());
+	}
+	
+	public static class ExpToStringConverter implements AST.Visitor<String> {
+		
+		public String visit(AST.AddExp e, Env env) {
+			String result = "(+ ";
+			for(AST.Exp exp : e.all()) 
+				result += exp.accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.Const e, Env env) {
+			return "" + e.v();
+		}
+		
+		public String visit(AST.DivExp e, Env env) {
+			String result = "(/ ";
+			for(AST.Exp exp : e.all()) 
+				result += exp.accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.ErrorExp e, Env env) {
+			return e.toString();
+		}
+		
+		public String visit(AST.MultExp e, Env env) {
+			String result = "(* ";
+			for(AST.Exp exp : e.all()) 
+				result += exp.accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.Program p, Env env) {
+			return "" + p.e().accept(this, env);
+		}
+		
+		public String visit(AST.SubExp e, Env env) {
+			String result = "(- ";
+			for(AST.Exp exp : e.all()) 
+				result += exp.accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.VarExp e, Env env) {
+			return "" + e.name();
+		}
+		
+		public String visit(AST.LetExp e, Env env) {
+			String result = "(let (";
+			List<String> names = e.names();
+			List<Exp> value_exps = e.value_exps();
+			int num_decls = names.size();
+			for (int i = 0; i < num_decls ; i++) {
+				result += " (";
+				result += names.get(i) + " ";
+				result += value_exps.get(i).accept(this, env) + ")";
+			}
+			result += ") ";
+			result += e.body().accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.LambdaExp e, Env env) {
+			String result = "(lambda (";
+			for(String formal : e.formals()) 
+				result += formal + " ";
+			result += ") ";
+			result += e.body().accept(this, env) + " ";
+			return result + ")";
+		}
+		
+		public String visit(AST.CallExp e, Env env) {
+			String result = "(";
+			result += e.operator().accept(this, env) + " ";
+			for(AST.Exp exp : e.operands())
+				result += exp.accept(this, env) + " ";
+			return result + ")";
+		}
 	}
 }
