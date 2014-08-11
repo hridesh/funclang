58078515957e28b391fc5116dfd77a2919762316
Hridesh Rajan <hridesh@cs.iastate.edu>
2014-08-10 22:15:00 -0500

    porting environment forward.

--- src/funclang/Env.java
+++ src/funclang/Env.java
@@ -1,5 +1,7 @@
 package funclang;
 
+import java.util.List;
+
 /**
  * Representation of an environment, which maps variables to values.
  * 
@@ -8,6 +10,7 @@ package funclang;
  */
 public interface Env {
 	Value get (String search_var);
+	void define (String saved_var, Value saved_val); //New for definelang
 
 	@SuppressWarnings("serial")
 	static public class LookupException extends RuntimeException {
@@ -16,9 +19,19 @@ public interface Env {
 		}
 	}
 	
+	@SuppressWarnings("serial")
+	static public class IllegalDefineException extends RuntimeException {
+		IllegalDefineException(String message){
+			super(message);
+		}
+	}
+
 	static public class EmptyEnv implements Env {
 		public Value get (String search_var) {
 			throw new LookupException("No binding found for name: " + search_var);
+		}
+		public void define(String saved_var, Value saved_val) {
+			throw new IllegalDefineException("Defining global variable " + saved_var + " not permitted in this language.");
 		}
 	}
 	
@@ -31,11 +44,41 @@ public interface Env {
 			_var = var;
 			_val = val;
 		}
-		public Value get (String search_var) {
+		public synchronized Value get (String search_var) {
 			if (search_var.equals(_var))
 				return _val;
 			return _saved_env.get(search_var);
 		}
+		public synchronized void define(String saved_var, Value saved_val) {
+			_saved_env.define(saved_var, saved_val);
+		}
 	}
 	
+	static public class GlobalEnv implements Env { // New for definelang
+		private Env _saved_env; 
+		private List<Binding> _bindings; 
+		public GlobalEnv() {
+			_saved_env = new EmptyEnv();
+			_bindings = new java.util.ArrayList<Binding>();
+		}
+		public synchronized Value get (String search_var) {
+			for(Binding binding : _bindings) {
+				if (search_var.equals(binding._var))
+					return binding._val;
+			}
+			return _saved_env.get(search_var);
+		}
+		public synchronized void define(String saved_var, Value saved_val) {
+			_bindings.add( new Binding(saved_var, saved_val));
+		}
+		private class Binding {
+			String _var; 
+			Value _val;
+			Binding(String var, Value val) {
+				_var = var;
+				_val = val; 
+			}
+		}
+	}
+
 }
