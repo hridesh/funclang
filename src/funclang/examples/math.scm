a9ea3d99350a5a94d3211bce1e8e1bc055db7bfd
Hridesh Rajan <hridesh@cs.iastate.edu>
2014-09-25 22:31:40 -0500

    Math functions.

---
+++ src/funclang/examples/math.scm
@@ -0,0 +1,20 @@
+
+(define even? 
+	(lambda (n)
+		(if (= n 0) #t
+			(if (odd? (- n 1)) #t
+				#f
+			)
+		)
+	)
+)
+
+(define odd? 
+	(lambda (n)
+		(if (= n 0) #f
+			(if (even? (- n 1)) #t
+				#f
+			)
+		)
+	)
+)
