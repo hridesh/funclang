e826a2f3163429262fd70c536adc82c232f1a62d
Hridesh Rajan <hridesh@cs.iastate.edu>
2015-01-31 13:14:50 -0600

    fix lib.

---
+++ src/funclang/lib/std.scm
@@ -0,0 +1,47 @@
+/* Library containing helpful definitions */
+
+(define i 1)
+(define ii 2)
+(define iii 3)
+(define iv 4)
+(define v 5)
+
+/* Additional list procedures with semantics similar to Scheme lists */
+
+(define cadr
+	(lambda (lst)
+		(car (cdr lst))
+	)
+)
+
+(define caddr
+	(lambda (lst)
+		(car (cdr (cdr lst)))
+	)
+)
+
+(define length
+	(lambda (lst)
+		(if (null? lst) 0
+			(+ 1 (length (cdr lst)))
+		)		
+	)
+)
+
+(define append
+	(lambda (lst1 lst2)
+		(if (null? lst1) lst2
+			(if (null? lst2) lst1
+				(cons (car lst1) (append (cdr lst1) lst2))
+			)
+		)		
+	)
+)
+
+(define map
+	(lambda (op lst)
+		(if (null? lst) (list)
+			(cons (op (car lst)) (map op (cdr lst)))
+		)		
+	)
+)
